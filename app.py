81508af8a4ade2e4d94cbccaad5db25f9da4a1e2
BakesBagels <79919353+BakesBagels@users.noreply.github.com>
2026-04-27 21:40:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from flask import Flask, make_response
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Fitness Tracker API - Secure Connection"
+    response = make_response("Fitness Tracker API - Secure Connection")
+    # Исправляем то, на что ругался ZAP:
+    response.headers['X-Frame-Options'] = 'DENY'  # Защита от кликджекинга
+    response.headers['X-Content-Type-Options'] = 'nosniff' # Защита от подмены типов
+    response.headers['Content-Security-Policy'] = "default-src 'self'" # CSP заголовок
+    response.headers['Server'] = 'SecureServer' # Скрываем версию сервера
+    return response
 
 if __name__ == "__main__":
-    # Для отчета: Исправление CWE-319 (SSL) реализовано через ssl_context.
-    # В среде CI/CD для работы DAST-сканера используем стандартный запуск.
-    # host='0.0.0.0' обязателен для работы внутри Docker-контейнера.
+    # Возвращаем host для Docker, но пока без SSL, чтобы ZAP не спотыкался
     app.run(host='0.0.0.0', port=5000)
